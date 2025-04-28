934625f8f4b72126ea723fc08bd8e4c7e9182d56
Phunzage <3143884145@qq.com>
2025-04-28 20:34:43 +0800

    eighteenth commit

---
+++ 4.Stack/linkedListStack.go
@@ -0,0 +1,18 @@
+// 基于链表实现栈
+
+package main
+
+import "container/list"
+
+// 基于链表实现的栈
+type linkedListStack struct {
+	// 使用内置包 list 实现栈
+	data *list.List
+}
+
+// 初始化栈
+func newlinkedListStack() *linkedListStack {
+	return &linkedListStack{
+		data: list.New(),
+	}
+}
