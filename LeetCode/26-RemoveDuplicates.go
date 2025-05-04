a2be90cdaadb5e03dfd0d189fa887715bbd5ae64
Phunzage <3143884145@qq.com>
2025-05-04 20:45:43 +0800

    leetcode-26.1

--- LeetCode/26-RemoveDuplicates.go
+++ LeetCode/26-RemoveDuplicates.go
@@ -1,14 +1,19 @@
 package leetcode
 
 func removeDuplicates(nums []int) int {
-	a := []int{}
-	for i := range nums {
-		for j := 0; j < len(a); j++ {
-			if i != j {
-				a[j] = i
-				break
+	if len(nums) == 0 {
+		return 0
+	}
+	last, finder := 0, 0
+	for last < len(nums)-1 {
+		for nums[finder] == nums[last] {
+			finder++
+			if finder == len(nums) {
+				return last + 1
 			}
 		}
+		nums[last+1] = nums[finder]
+		last++
 	}
-	return len(a)
+	return last + 1
 }
