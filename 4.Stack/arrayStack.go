acfddf075e82cdfb3ff35bbec8a2d40d4ff53d60
Phunzage <3143884145@qq.com>
2025-05-05 11:22:45 +0800

    arrayStack-2

--- 4.Stack/arrayStack.go
+++ 4.Stack/arrayStack.go
@@ -6,3 +6,21 @@ package main
 type arrayStack struct {
 	data []int // 数据
 }
+
+// 初始化栈
+func newArrayStack() *arrayStack {
+	return &arrayStack{
+		// 设置栈的长度为0，容量为16
+		data: make([]int, 0, 16),
+	}
+}
+
+// 栈的长度
+func (s *arrayStack) size() int {
+	return len(s.data)
+}
+
+// 栈是否为空
+func (s *arrayStack) isEmpty() bool {
+	return s.size() == 0
+}
