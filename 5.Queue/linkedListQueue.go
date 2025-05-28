000cb94b934afc1459e72578205410674d5127f4
Phunzage <3143884145@qq.com>
2025-05-28 18:25:28 +0800

    queue-2

--- 5.Queue/linkedListQueue.go
+++ 5.Queue/linkedListQueue.go
@@ -31,7 +31,26 @@ func (s *linkedListQueue) pop() any {
 	return e.Value
 }
 
+/* 访问队首元素 */
+func (s *linkedListQueue) peek() any {
+	if s.isEmpty() {
+		return nil
+	}
+	e := s.data.Front()
+	return e.Value
+}
+
+/* 获取队列的长度 */
+func (s *linkedListQueue) size() int {
+	return s.data.Len()
+}
+
 /* 判断队列是否为空 */
 func (s *linkedListQueue) isEmpty() bool {
 	return s.data.Len() == 0
+}
+
+/* 获取 List 用于打印 */
+func (s *linkedListQueue) toList() *list.List {
+	return s.data
 }
--- 5.Queue/main.go
+++ 5.Queue/main.go
@@ -1,10 +1,36 @@
 package main
 
+import (
+	"container/list"
+	"fmt"
+)
+
+// 初始化队列
+// 在Go中，将 list 作为队列来使用
 func main() {
-	// 初始化队列
-	// 在Go中，将 List 作为队列来使用
-	queue := List.New()
+
+	queue := list.New()
 
 	// 元素入队
+	queue.PushBack(1)
+	queue.PushBack(3)
+	queue.PushBack(2)
+	queue.PushBack(5)
+	queue.PushBack(4)
 
+	// 访问队首元素
+	peek := queue.Front()
+	fmt.Println(peek)
+
+	// 元素出队
+	pop := queue.Front()
+	queue.Remove(pop)
+
+	// 获取队列的长度
+	size := queue.Len()
+	fmt.Println(size)
+
+	// 判断队列是否为空
+	isEmpty := queue.Len() == 0
+	fmt.Println(isEmpty)
 }
